0c422105693d09730cf7df48082eacc6ae52921a
zackabdull <zakaria.maalim@student.moringaschool.com>
2021-11-21 23:36:12 +0300

    initial commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $(".clickable").click(function () {
+    $("#design-img").toggle("slow");
+    $("#design-txt").toggle("slow");
+  });
+});
+$(document).ready(function () {
+  $(".clickable2").click(function () {
+    $("#development-img").toggle("slow");
+    $("#development-txt").toggle("slow");
+  });
+});
+$(document).ready(function () {
+  $(".clickable3").click(function () {
+    $("#product-img").toggle("slow");
+    $("#product-txt").toggle("slow");
+  });
+});
