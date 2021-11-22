38fab3a156a6bf1129e1f384679adffc8cb0a1d2
zackabdull <zakaria.maalim@student.moringaschool.com>
2021-11-22 12:42:47 +0300

    add the form section

--- js/app.js
+++ js/app.js
@@ -16,3 +16,29 @@ $(document).ready(function () {
     $("#product-txt").toggle("slow");
   });
 });
+$(document).ready(function () {
+  $("form#form1").on("submit", function (event) {
+    event.preventDefault();
+    var name = $("input#username").val();
+    var email = $("input#email").val();
+    var message = $("textarea#message").val();
+
+    if (
+      $("input#username").val() &&
+      $("input#email").val() &&
+      $("input#message").val()
+    ) {
+      alert(
+        "Hi " +
+          name +
+          ", Thank you for reaching out, we'll get back to you shortly..."
+      );
+    } else if ($("input#username").val() && $("input#email").val()) {
+      alert(
+        "Thank you for reaching ou to us. If you wish to leave a message please type in on the message section.."
+      );
+    } else {
+      alert("Please provide correct name or email");
+    }
+  });
+});
